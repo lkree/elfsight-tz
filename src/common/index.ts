8db94bb80bfb158bd055e2b18471244c9be0b634
ll.dugenec <xtendmix@gmail.com>
2021-09-24 14:28:58 +0300

    deleted unneeded type + fixed dependencies

--- src/common/index.ts
+++ src/common/index.ts
@@ -1,6 +1,4 @@
-export type {
-    Gender, Status, Species, ICharacter, ILoadResult, ILoadMeta, ParamsWithOutDispatch, SomeFunction
-} from './interfaces';
+export type { Gender, Status, Species, ICharacter, ILoadResult, ILoadMeta } from './interfaces';
 export { getCurrentPageNumber,getUrlWithPageNumber, getUrlWithFilters } from './utils';
 export type { IFilter, TInputFilter, TSelectFilter } from './filters';
 export { COMMON_URL, CHARACTER_URL } from './constants';
--- src/common/interfaces.ts
+++ src/common/interfaces.ts
@@ -1,13 +1,8 @@
-import { AppDispatch } from '../store';
-
 export type Gender = 'male' | 'female' | 'genderless' | 'unknown';
 
 export type Species = 'Humanoid';
 
 export type Status = 'Dead' | 'Alive' | 'unknown';
-
-export type SomeFunction = (...args: any) => any;
-export type ParamsWithOutDispatch<T extends SomeFunction> = T extends (dispatch: AppDispatch, ...args: infer P) => any ? P : never;
 
 interface ILink {
     name: string;
